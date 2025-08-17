cb3ab1c75b795419c7d0aab17ad4a66a50548f36
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2025-08-17 22:58:56 +1000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-kill $(ps aux | grep -E 'bot.sh|monitor.sh' | grep -v grep | awk '{print $2}')
-sleep 2
+# Handle bot.sh processes
+bot_pids=$(ps aux | grep '[b]ot.sh' | awk '{print $2}')
+if [ -n "$bot_pids" ]; then
+  kill $bot_pids
+  echo "Killed bot.sh processes:"
+  echo "$bot_pids"
+  echo ""
+else
+  echo "No bot.sh processes were active"
+  echo ""
+fi
+
+# Handle monitor.sh processes
+monitor_pids=$(ps aux | grep '[m]onitor.sh' | awk '{print $2}')
+if [ -n "$monitor_pids" ]; then
+  kill $monitor_pids
+  echo "Killed monitor.sh processes:"
+  echo "$monitor_pids"
+  echo ""
+else
+  echo "No monitor.sh processes were active"
+  echo ""
+fi
+
+# Show there are no remaining processes
+echo "The only remaining processes are as follows (If all processes have been killed it is normal to see entries related to grep):"
 ps aux | grep bot.sh
 ps aux | grep monitor.sh
