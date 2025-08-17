30888e9e43ec0d8d8a0b876fe041f78371cff335
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2025-08-17 23:20:36 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,8 @@
 
 nohup ./bot.sh > bot_output.log 2>&1 &
 nohup ./monitor.sh > monitor_output.log 2>&1 &
+
+echo ""
+echo "XDC ShopBot - Telegram Bot Started (bot.sh)"
+echo "XDC Shopbot - Blockchain Payment Monitor Started (monitor.sh)"
+echo ""
